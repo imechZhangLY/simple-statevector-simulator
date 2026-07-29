598eca83c02a13b80714229d385834ce29f331ba
zhangluyao <zhangluyao@DESKTOP-UAEJP75.localdomain>
2026-07-30 07:50:17 +0800

    add permute experiments

--- experiments/test-reshpae-supa.py
+++ experiments/test-reshpae-supa.py
@@ -10,9 +10,20 @@ for num_qubits in [16, 20, 24, 28]:
         1 << num_qubits, dtype=torch.complex64, device=torch.device("supa")
     )
     statevector[0] = 1.0
+    qubits = [num_qubits - 1]
+    target_axes = [num_qubits - 1 - qubit for qubit in qubits]
+    remaining_axes = [
+        axis for axis in range(num_qubits) if axis not in target_axes
+    ]
+    axes = target_axes + remaining_axes
+    inverse_axes = [0] * num_qubits
+    for position, axis in enumerate(axes):
+        inverse_axes[axis] = position
     start_time = perf_counter()
     for i in range(REPEATS):
         updated = statevector.reshape((2, ) * num_qubits)
+        updated = updated.permute(axes).reshape(2, -1)
+        updated = updated.reshape((2,) * num_qubits).permute(inverse_axes).reshape(-1)
     
     cost_before_sync = (perf_counter() - start_time) / REPEATS * 1000
     torch.supa.synchronize()
